e986a0619ca6273f75ff2c9b284553464e7ac349
Michael <micperdo@gmail.com>
2022-12-06 10:47:19 -0500

    creates 11 express routes and request handlers, 6x GET, 2x POST, 1x PUT, 2x DELETE

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express"),
   path = require("path"),
   morgan = require("morgan"),
+  bodyParser = require("body-parser"),
   fs = require("fs");
+
 const app = express();
 const port = 5000;
+
+// 'in memory data' to test the request handlers
 let top10 = [
   {
     name: "Knives Out",
@@ -11,6 +15,7 @@ let top10 = [
     releaseYear: 2019,
     studio: "Lionsgate",
     country: "us",
+    genre: ["mystery", "comedy"],
   },
   {
     name: "Lola Rennt",
@@ -18,20 +23,16 @@ let top10 = [
     releaseYear: 1998,
     studio: "Prokino Filmverleih",
     country: "de",
+    genre: ["sci-fi"],
   },
-  {
-    name: "Lola Rennt",
-    director: "Tom Twyker",
-    releaseYear: 1998,
-    studio: "Prokino Filmverleih",
-    country: "de",
-  },
+
   {
     name: "In Bruges",
     director: "Martin McDonagh",
     releaseYear: 2008,
     studio: ["Focus Features", "Universal"],
     country: ["uk", "us"],
+    genre: ["comedy", "dark-comedy"],
   },
   {
     name: "Godfather",
@@ -39,6 +40,7 @@ let top10 = [
     releaseYear: 1978,
     studio: "MGM",
     country: "us",
+    genre: ["drama"],
   },
   {
     name: "The Green Knight",
@@ -46,36 +48,22 @@ let top10 = [
     releaseYear: 2021,
     studio: "A24",
     country: "us",
+    genre: ["fantasy", "historical", "drama"],
   },
   {
-    name: "Lola Rennt",
-    director: "Tom Twyker",
-    releaseYear: 1998,
-    studio: "Prokino Filmverleih",
-    country: "de",
-  },
-  {
-    name: "Lola Rennt",
-    director: "Tom Twyker",
-    releaseYear: 1998,
-    studio: "Prokino Filmverleih",
-    country: "de",
-  },
-  {
-    name: "Lola Rennt",
-    director: "Tom Twyker",
-    releaseYear: 1998,
-    studio: "Prokino Filmverleih",
-    country: "de",
-  },
-  {
-    name: "Lola Rennt",
-    director: "Tom Twyker",
-    releaseYear: 1998,
-    studio: "Prokino Filmverleih",
-    country: "de",
+    name: "The Batman",
+    director: "Matt Reeves",
+    releaseYear: 2022,
+    studio: "Warner Bros",
+    country: "us",
+    genre: ["superhero", "crime"],
   },
 ];
+
+let genres = [];
+top10.forEach((movie) => {
+  movie.genre.forEach((genre) => genres.push(genre));
+});
 
 // creates a write stream for morgan to log each request to the server in logs file.
 let logStream = fs.createWriteStream(path.join(__dirname, "logs.txt"), {
@@ -85,23 +73,95 @@ let logStream = fs.createWriteStream(path.join(__dirname, "logs.txt"), {
 // middleware
 app.use(morgan("common", { stream: logStream }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 //error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).send("Something broken in application");
 });
 
-app.get("/", (req, res) => {
-  res.send(`<h3> Welcome to the movie api </h3>`);
+// API Endpoints and request handlers
+app.get("/movies", (req, res) => {
+  res.status(200).json(top10);
+});
+// get data about a specific movie
+app.get("/movies/:title", (req, res) => {
+  let title = req.params.title;
+  res.status(200).json(top10.find((movie) => movie.name === title));
+});
+//get a list of all directors
+app.get("/directors", (req, res) => {
+  res.status(200).send(top10.map((movie) => movie.director));
+});
+// get data about a specific director
+app.get("/directors/:name", (req, res) => {
+  let directors = top10.map((movie) => movie.director);
+  res
+    .status(200)
+    .json(directors.filter((director) => director === req.params.name));
+});
+// get all genres
+app.get("/genres", (req, res) => {
+  res.status(200).json(genres);
+});
+// get movies with a specific genre type
+app.get("/genres/:type", (req, res) => {
+  res
+    .status(200)
+    .send(top10.filter((movie) => movie.genre.includes(req.params.type)));
 });
 
 app.get("/documentation.html", (req, res) => {
   res.sendFile(path.join(__dirname, "documentation.html"));
 });
+// add new user
+app.post("/users", (req, res) => {
+  let newAccount = req.body;
+  console.log(newAccount);
+  if (!newAccount.name) {
+    res.status(400).json({ message: "error, request must have name variable" });
+  } else {
+    res
+      .status(201)
+      .json({ message: "successfully added new account", data: newAccount });
+  }
+});
+// update a username
+app.put("/users/:username", (req, res) => {
+  let username = req.params.username;
+  let data = req.body;
+  if (!data.newUsername) {
+    res.status(404).send("A new username was not submitted");
+  } else {
+    res
+      .status(200)
+      .send(`Username changed from ${username} to ${data.newUsername}`);
+  }
+});
+// add new movie to user's favorites list.
+app.post("/users/:username/favorites/:movie", (req, res) => {
+  let newMovie = req.params.movie;
+  let user = req.params.username;
+  res
+    .status(200)
+    .send(`${newMovie} successfully added to ${user}'s favorites.`);
+});
 
-app.get("/movies", (req, res) => {
-  res.json(top10);
+// delete a movie from favorites list.
+app.delete("/users/:username/favorites/:movie", (req, res) => {
+  let deleteMovie = req.params.movie;
+  let user = req.params.username;
+  res
+    .status(200)
+    .send(`${deleteMovie} successfully deleted from ${user}'s favorites.`);
+});
+
+// delete user's account
+app.delete("/users/:username", (req, res) => {
+  res
+    .status(200)
+    .send(`${req.params.username}'s account successfully deleted. `);
 });
 
 app.listen(port, () => {
