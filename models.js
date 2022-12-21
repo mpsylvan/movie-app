df76fecf1e2af88eb24bdbbe15d49767d9af6e0e
Michael <micperdo@gmail.com>
2022-12-21 14:39:20 -0500

    updates models.js to conform with uppercase db keys

--- models.js
+++ models.js
@@ -3,54 +3,53 @@ const bcrypt = require("bcrypt");
 
 let movieSchema = mongoose.Schema({
   // calling the Schema constructor on the mognoose module, and passing in the schema/ template with data types, and requirements.
-  name: { type: String, required: true },
-  director: {
+  Name: { type: String, required: true },
+  Director: {
     // director is a sub-document.
-    name: { type: String, required: true },
-    nationality: { type: String, required: true },
-    bio: { type: String, required: true },
+    Name: { type: String, required: true },
+    Nationality: { type: String, required: true },
+    Bio: { type: String, required: true },
   },
-  releaseYear: { type: Number, required: true },
-  studio: { type: String, required: true },
-  synopsis: { type: String, required: true },
-  country: { type: String, required: true },
-  genre: {
+  ReleaseYear: { type: Number, required: true },
+  Studio: { type: String, required: true },
+  Synopsis: { type: String, required: true },
+  Country: { type: String, required: true },
+  Genre: {
     // genre is a sub-document.
-    name: String,
-    description: String,
+    Name: String,
+    Description: String,
   },
-  feature: Boolean,
+  Feature: Boolean,
 });
 // User schema declaration
 let userSchema = mongoose.Schema({
-  name: String,
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  username: { type: String, required: true },
-  birthdate: Date,
-  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+  Email: { type: String, required: true },
+  Password: { type: String, required: true },
+  Username: { type: String, required: true },
+  Birthdate: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
-// static method attached to the User schema that hashes the password of each instance of User document.
-userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10); // the string to be hashed and the salt
+// static method attached to the User schema that hashes the password of each instance of User document.nor
+userSchema.statics.hashPassword = (Password) => {
+  return bcrypt.hashSync(Password, 10); // the string to be hashed and the salt
 };
 
 // instance method compares an inputted password with the User document password field that is in context.
-userSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.password);
+userSchema.methods.validatePassword = function (Password) {
+  return bcrypt.compareSync(Password, this.Password);
 };
 
 let directorSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  bio: { type: String, required: true },
-  nationality: String,
-  dateOfBirth: { type: Date, required: true },
+  Name: { type: String, required: true },
+  Bio: { type: String, required: true },
+  Nationality: String,
+  DateOfBirth: { type: Date, required: true },
 });
 
 let genreSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
+  Name: { type: String, required: true },
+  Description: String,
 });
 
 // create create the models, passing in the model name (this gets pluralized and lowercased), and the schema that that model will follow.
