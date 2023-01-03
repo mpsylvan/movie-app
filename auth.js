a0381bda9df3d6adeaa734692c79be5b62bfcba8
Michael <micperdo@gmail.com>
2023-01-03 12:25:21 -0500

    replaces !user condition in local strategy if condition

--- auth.js
+++ auth.js
@@ -16,7 +16,7 @@ let generateJWTToken = (user) => {
 module.exports = (router) => {
   router.post("/login", (req, res) => {
     passport.authenticate("local", { session: false }, (error, user, info) => {
-      if (error) {
+      if (error || !user) {
         return res.status(400).json({
           message: "login issue detected",
           user: user,
